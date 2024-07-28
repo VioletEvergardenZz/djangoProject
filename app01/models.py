20d8f5d4f237395486757592f5249e1821cfda05
VioletEvergardenZz <1561862432@qq.com>
2024-07-28 15:56:29 +0800

    类创建表

--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# 使用命令行 python manage.py makemigrations 和  python manage.py migrate
+class UserInfo(models.Model):
+    name = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    age = models.IntegerField(default=2)
+
+class Department(models.Model):
+    title = models.CharField(max_length=16)
+
+# class Role(models.Model):
+#     caption = models.CharField(max_length=16)
